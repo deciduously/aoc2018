9c6211da655ced2e63a120c063e42ce8438168ff
deciduously <bendlovy@gmail.com>
2018-12-07 07:31:40 -0500

    Working grid

--- src/Day6/Library.fs
+++ src/Day6/Library.fs
@@ -13,7 +13,6 @@ module util =
   type Coord = int * (int * int)
   let getX = snd >> fst
   let getY = snd >> snd
-  let eqCoord origin target = fst origin = fst target && snd origin = snd target
 
   let manhattanDistance (origin: int * int) (target: int * int) =
     System.Math.Abs (fst origin - fst target) + System.Math.Abs (snd origin - snd target)
@@ -21,26 +20,37 @@ module util =
   let gridSize (points: Coord list) =
     let across = List.map getX points |> List.sort |> List.rev |> List.head
     let down = List.map getY points |> List.sort |> List.rev |> List.head
-    (across, down)
+    (across + 1, down + 1)
   
   let distanceGrid points =
     let (across, down) = gridSize points
     Array2D.mapi (fun down across c ->
       let nearest =
         points
-        |> List.fold (fun s el -> Map.add (fst el) (manhattanDistance (across, down) (snd el)) s) (new Map<int, int> (Seq.empty))
-        |> Map.fold (fun s k v -> if v < snd s then (k, v) else s) (across, System.Math.Max(across, down))
-      if List.exists (fun el -> eqCoord (snd el) (across, down)) points then labels.[fst nearest + 26] else labels.[fst nearest]) (Array2D.create down across '.')
+        // get all distances
+        |> List.fold (fun s el -> Map.add (fst el) (manhattanDistance (snd el) (across, down)) s) (new Map<int, int> (Seq.empty))
+        // get nearest distance
+        |> Map.fold (fun (s: (int * int) list) k v -> if v < snd s.[0] then [(k, v)] else if v = snd s.[0] then s @ [(k, v)] else s) [0, across * down]
+      if List.exists (fun el -> (snd el) = (across, down)) points then
+        labels.[fst nearest.[0] + 26]
+      else
+        if List.length nearest > 1 then
+          c
+        else
+          labels.[fst nearest.[0]]) (Array2D.create down across '.')
   
-  let tickMap c (map: Map<char, int>) =
-    match (map.TryFind c) with
-    | Some x -> Map.add c (x + 1) map
-    | None -> Map.add c 1 map
+  let tickMap c (map: Map<char, int>) edge =
+    if edge then
+      Map.add c 0 map
+    else
+      match (map.TryFind c) with
+      | Some x -> Map.add c (x + 1) map
+      | None -> Map.add c 1 map
 
   let largetArea distanceGrid =
     distanceGrid
     |> Seq.cast<char>
-    |> Seq.fold (fun s el -> tickMap el s) (new Map<char, int> (Seq.empty))
+    |> Seq.fold (fun s el -> tickMap el s false) (new Map<char, int> (Seq.empty))
     |> Map.toList
     |> List.sortBy snd
     |> List.head
@@ -75,5 +85,5 @@ module run =
   let runBoth =
     printfn "Sample:"
     displayResults sample
-    printfn "Real:"
-    displayResults real
+    //printfn "Real:"
+    //displayResults real
